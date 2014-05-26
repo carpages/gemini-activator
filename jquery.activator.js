97b9d61b92714fba5680e23af4ffabe6844b5d79
Matt Rose <mrose@carpages.ca>
2014-05-26 10:03:57 -0400

    Updated to use jquery-boiler

--- jquery.activator.js
+++ jquery.activator.js
@@ -1,130 +1,84 @@
-define(['jquery-loader', 'underscore'], function($, _){
+define(['jquery-loader', 'underscore', 'jquery.boiler'], function($, _){
 
-	$.activator = function(element, options){
+  $.boiler('activator', {
+    defaults: {
+      target: 'this',
+      activeClass: 'is-active',
+      checkbox: false,
+      once: false,
+      event: 'click',
+      preventDefault: true
+    },
 
-		// plugin's default options
-        var defaults = {
-			target: 'this',
-			activeClass: 'is-active',
-			checkbox: false,
-			once: false,
-			event: 'click',
-			preventDefault: true
-        };
+    init: function(){
+      var plugin = this;
 
-        // to avoid confusions, use "base" to reference the current instance of the object
-		var base = this;
+      //cache the targets
+      plugin.targets = plugin._getTargets();
 
-		// plugin's properties will be available through:
-        // base.settings.propertyName from inside the plugin or
-        // element.data('activator').settings.propertyName from outside the plugin
-		base.settings = $.extend({}, defaults, options);
+      // bind the event to toggle the class
+      plugin.$el.bind(plugin.settings.event, function(e){
+        if(plugin.settings.preventDefault) e.preventDefault();
 
-		// cache / reference to the jQuery version of DOM elements
-		var $element = $(element);
+        // toggle the class on each target element
+        _.each(targets, function(target){
+          target.$el.toggleClass(target.activeClass);
 
-		// private methods
-        // -------------------------------------------
-        // -------------------------------------------
+          // tick checkbox
+          if (plugin.settings.checkbox){
+            target.$el.find(':checkbox').prop('checked',
+              target.$el.hasClass(target.activeClass)
+            );
+          }
+        });
 
-		// extract the targets and their active state from the settings
-		var getTargets = function() {
+        // unbind
+        if(plugin.settings.once) plugin.$el.unbind(plugin.settings.event);
+      });
 
-			var targets = [];
-			var addTarget = function(target, activeClass){
-				targets.push(
-					{
-						$el: target == "this" ? $element : $(target),
-						activeClass: activeClass
-					}
-				);
-			};
+    },
 
-			if (_.isArray(base.settings.target)) {
+    _getTargets: function() {
+      var plugin = this;
 
-				_.each(base.settings.target, function(target){
+      var targets = [];
+      var addTarget = function(target, activeClass){
+        targets.push(
+          {
+            $el: target == 'this' ? plugin.$el : $(target),
+            activeClass: activeClass
+          }
+        );
+      };
 
-					addTarget(target, base.settings.activeClass);
+      if (_.isArray(plugin.settings.target)) {
 
-				});
+        _.each(plugin.settings.target, function(target){
 
-			} else if (_.isObject(base.settings.target)) {
-
-				_.each(base.settings.target, function(activeClass, target){
-
-					addTarget(target, activeClass);
-
-				});
-
-			} else {
-
-				addTarget(base.settings.target, base.settings.activeClass);
-
-			}
-
-			return targets;
-		};
-
-		//cache the targets
-		var targets = getTargets();
-
-		// the method that initiates DOM listeners and manipulation
-		var init = function(){
-
-			// bind the event to toggle the class
-			$element.bind(base.settings.event, function(e){
-				if(base.settings.preventDefault) e.preventDefault();
-
-				// toggle the class on each target element
-				_.each(targets, function(target){
-					target.$el.toggleClass(target.activeClass);
-
-					// tick checkbox
-					if (base.settings.checkbox){
-						target.$el.find(':checkbox').prop('checked',
-							target.$el.hasClass(target.activeClass)
-						);
-					}
-				});
-				// unbind
-				if(base.settings.once) $element.unbind(base.settings.event);
-			});
-
-		};
-
-		// fire up the plugin!
-		init();
-
-	};
-
-	// add the plugin to the jQuery.fn object
-    $.fn.activator = function(method) {
-
-        // iterate through the DOM elements we are attaching the plugin to
-        return this.each(function() {
-
-            // if plugin has not already been attached to the element
-            if (undefined === $(this).data('activator')) {
-
-                // create a new instance of the plugin
-                // pass the user-provided "method" (assumed to be an object of options)
-                var plugin = new $.activator(this, method);
-
-                // store a reference to the plugin object in case user needs access to public variables and methods
-                $(this).data('activator', plugin);
-
-            // if the plugin is already initiated, check to see if the method exists
-            } else if ($(this).data('activator')[method]) {
-				// call the method
-				$(this).data('activator')[method].apply( this, arguments );
-            }
+          addTarget(target, plugin.settings.activeClass);
 
         });
 
-    };
+      } else if (_.isObject(plugin.settings.target)) {
 
-    // Return the jquery object
-    // This way you don't need to require both jquery and the plugin
-    return $;
+        _.each(plugin.settings.target, function(activeClass, target){
+
+          addTarget(target, activeClass);
+
+        });
+
+      } else {
+
+        addTarget(plugin.settings.target, plugin.settings.activeClass);
+
+      }
+
+      return targets;
+    }
+  });
+
+  // Return the jquery object
+  // This way you don't need to require both jquery and the plugin
+  return $;
 
 });
