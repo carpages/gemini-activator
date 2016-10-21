eb623969401e46237f4e5acf087433a93c0ad113
Jeff Andrews <jeff.andrews@carpages.ca>
2016-10-21 15:22:30 -0400

    Fix CommonJS require statement to require the correct gemini-loader module

--- gemini.activator.js
+++ gemini.activator.js
@@ -44,7 +44,7 @@ to add and remove the ``is-active`` class.
   } else if ( typeof exports === 'object' ) {
      // Node/CommonJS
     module.exports = factory(
-      require( 'gemini' )
+      require( 'gemini-loader' )
     );
   } else {
      // Browser globals
